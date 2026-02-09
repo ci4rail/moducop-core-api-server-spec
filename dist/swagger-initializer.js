41caee22d5678cd9a136b7511a0e459c67a3f3db
popples <klauspopp@gmx.de>
2026-02-09 15:11:32 +0100

    fix build

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "sio06-apiv1.yml",
+    url: "core-api-server.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
